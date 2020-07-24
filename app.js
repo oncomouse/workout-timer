ae16126bfe66154a7e25044c3041e7451d0f6b67
oncomouse <oncomouse@gmail.com>
2020-07-24 11:15:32 -0500

    WOrk

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 /* globals R, $, Bacon */
 Bacon.$.init($)
 $(function () {
-  var WORKOUT = {
+  $('body').flowtype()
+  var WORKOUT = $('#workout').length === 0 ? {
     rest: 0,
     reps: 1,
     setTime: 50,
     exercises: [],
-  }
+  } : JSON.parse($('#workout').text())
   var BUTTON_STOP_TEXT = 'Click to Stop Workout'
   var BUTTON_START_TEXT = 'Click to Start Workout'
   $('<button />')
     .text(BUTTON_START_TEXT)
     .attr('id', 'startButton')
+    .addClass('fixed ma2 z-2')
+    .css('top', 0)
+    .css('left', 0)
     .appendTo($('#root'))
+  $('<div />')
+    .attr('id', 'container')
+    .addClass('absolute--fill tc')
+    .appendTo($('#root'))
+  $('<h2 />')
+    .attr('id', 'exercise')
+    .addClass('b')
+    .css('font-size', '5em')
+    .appendTo($('#container'))
+  $('<h1 />')
+    .attr('id', 'timer')
+    .addClass('b')
+    .css('font-size', '7em')
+    .appendTo($('#container'))
   var timer = Bacon.interval(1000)
   var startButtonStream = $('#startButton').asEventStream('click')
   startButtonStream.onValue(function (ev) {
@@ -25,6 +43,6 @@ $(function () {
     [timer, R.ifElse(R.is(Number), R.add(1), R.identity)]
   )
   tickProperty.filter(R.is(Number)).onValue(function (tick) {
-
+    $('#timer').text(tick)
   })
 })
